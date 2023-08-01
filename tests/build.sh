3394dc6992bd5f907f687897d3c7e4d1bfb83e0c
Muthiah131998 <110098347+Muthiah131998@users.noreply.github.com>
2023-08-01 13:13:49 +0530

    Update build.sh

--- tests/build.sh
+++ tests/build.sh
@@ -1,2 +1,2 @@
 cp distribution/docker/Dockerfile .
-docker buildx build . --output type=docker,name=elestio4test/dremio-oss:latest | docker load
+docker buildx build . --build-arg DOWNLOAD_URL=dremio-community-LATEST.tar.gz --output type=docker,name=elestio4test/dremio-oss:latest | docker load
