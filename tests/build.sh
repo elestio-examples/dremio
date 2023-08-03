9d244af9109cbf744db623cb00dfbe8916f20e88
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-08-03 13:12:20 +0300

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,2 +1,7 @@
 cp distribution/docker/Dockerfile .
-docker buildx build . --build-arg DOWNLOAD_URL=https://download.dremio.com/community-server/dremio-community-LATEST.tar.gz --output type=docker,name=elestio4test/dremio:latest | docker load
+
+sed -i 's~ARG DOWNLOAD_URL~~g' ./Dockerfile
+sed -i 's~&& wget -q "\${DOWNLOAD_URL}" -O dremio.tar.gz~&& wget -q "https://download.dremio.com/community-server/dremio-community-LATEST.tar.gz" -O dremio.tar.gz~g' ./Dockerfile
+
+
+docker buildx build . --output type=docker,name=elestio4test/dremio:latest | docker load
